df4659c1acd69132967069b4b3ab6b7c7d79170e
Nishchal Acharya <140315374+Nishchal-ll@users.noreply.github.com>
2025-12-25 20:44:24 +0545

    Create q3-a.java

---
+++ q3-a.java
@@ -0,0 +1,13 @@
+package javaq;
+
+/**
+ *
+ * @author nishchal
+ */
+public class Main {
+    public static void main(String [] args){
+int [] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+int sum= array[2]+array[14];
+System.out.println("The sum is "+sum);
+    }
+}
